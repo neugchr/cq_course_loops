be96abb520f8accfe7ceb0e2b1c988478be872d3
Seminar Bioinformatik <neugchr>
2022-02-17 10:32:27 +0100

    added loops.py and a gitignore

--- loops.py
+++ loops.py
@@ -1,1 +1,23 @@
 
+sum = 0
+for i in range(0,22):
+    sum += i
+print(sum)
+
+sum = 0
+for i in range(0,22):
+    if i%2 == 0:
+        sum += i
+print(sum)
+
+lst = []
+for i in range(0,22):
+    lst.insert(0, i)
+print(lst)
+
+sum = 0
+for c in 'abcdefghijklmnopqrstuv':
+    print(c)
+    sum += ord(c)-97 # convert character into number and subtract offset
+    print(sum)
+print(sum)
